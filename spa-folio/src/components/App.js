39bf2887be499cc6df6b047554999ba2e903cb4e
Supp-2020 <supreetpradhan@gmail.com>
2024-07-21 21:19:54 +0530

    Folder structure added

--- spa-folio/src/components/App.js
+++ spa-folio/src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Navigation from './Navigation/nav'
+import Dashboard from './Dashboard/dashboard'
+import Experience from './Experience/experience'
+import Project from './Project/project'
+import Technology from './Technology/technology'
+import Contact from './Contact/contact'
+import Footer from './Footer/footer'
 
 function App() {
   return (
-    <div>App</div>
+    <div id='App'>
+      <Navigation />
+      <Dashboard />
+      <Experience />
+      <Project />
+      <Technology />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
